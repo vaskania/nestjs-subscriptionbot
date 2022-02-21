17be7ce74a97484256d27bb1eff14dc779b81376
vaskania <vnikabadze@unisens.ge>
2022-02-21 17:44:56 +0400

    testing axios

--- src/forecast/weather.service.ts
+++ src/forecast/weather.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from "@nestjs/common";
 import { HttpService } from "@nestjs/axios";
 import { Observable } from "rxjs";
 
+const API = "64f266fb3ffa0f2b1da717bf3720157e";
+
 @Injectable()
 export class WeatherService {
   constructor(private httpService: HttpService) {
   }
-  //
-  // uri = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API}&units=metric`;
-  //
+
+  setUrl = (latitude, longitude) => {
+    return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API}&units=metric`;
+  };
+
+
+
   // findAll(): Observable<AxiosResponse<Cat[]>> {
-  //   return this.httpService.get(this.uri);
+  //   return this.httpService.get(this.setUrl());
   // }
 }
